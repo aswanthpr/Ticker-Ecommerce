6fc47148d1df43f1ea0ccfe23ef29915b0ce21f2
aswanthpr <aswanthpr515@gmail.com>
2024-07-08 18:48:51 +0530

    fix edit address buh

--- helper/googleAuth.js
+++ helper/googleAuth.js
@@ -37,7 +37,7 @@ passport.deserializeUser((id,done)=>{
         new googleStratagy({
            clientID: process.env.CLIENT_ID,
            clientSecret:process.env.CLIENT_SECRET,
-           callbackURL:"http://localhost:3000/google/callback",
+           callbackURL:"/google/callback",
             passReqToCallback:true,
         },
        async function (req,accessToken ,refreshToken, profile,done){
